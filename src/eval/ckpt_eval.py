b589911fd2e1ba7a901bdb8d20ad72213959b282
sendeniz <denizsen@protonmail.com>
2026-03-24 11:14:08 +0100

    ckpt eval

--- src/eval/ckpt_eval.py
+++ src/eval/ckpt_eval.py
@@ -238,12 +238,13 @@ def main(args):
                                         pos_thresholds=config['eval']['pos_thresholds'],
                                         iou_threshold_range=config['eval']['iou_thresholds'],
                                         angular_thresholds=config['eval']['angular_thresholds'],
-                                        match_pairs=config['eval']['match_pairs']))
+                                        match_pairs=config['eval']['match_pairs'])
         
         post_test_metrics = get_eval_metrics(post_test_preds, test_gts, 
                                         pos_thresholds=config['eval']['pos_thresholds'],
                                         iou_threshold_range=config['eval']['iou_thresholds'],
-                                        angular_thresholds=config['eval']['angular_thresholds'])
+                                        angular_thresholds=config['eval']['angular_thresholds'],
+                                        match_pairs=config['eval']['match_pairs'])
         
         print_evaluation_results(test_metrics, post_test_metrics)
 
